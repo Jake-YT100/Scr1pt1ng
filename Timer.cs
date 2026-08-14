997b7dfaaad15347032361108cd3a5167397f9e4
Jake_X100 <trail.fur@yandex.ru>
2026-08-14 16:18:49 +0300

    Add files via upload

--- Timer.cs
+++ Timer.cs
@@ -1,27 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
+    public TextMeshProUGUI timerText;
     public int minutes;
     public float seconds;
     private void Update()
     {
-        seconds = Time.deltaTime;
-        if (seconds == 0)
+        seconds -= Time.deltaTime;
+        if (seconds <= 0)
         {
-            if (minutes == 0)
+            if (minutes > 0)
+            {
+                seconds += 59;
+                minutes--;
+            }
+            else
             {
                 int sceneIndex = SceneManager.GetActiveScene().buildIndex;
                 SceneManager.LoadScene(sceneIndex);
             }
-            else if (minutes > 0)
-            {
-                seconds =+ 59;
-                minutes =+ 1;
-            }
-        }    
+            
+        }
+        int roundSeconds = Mathf.RoundToInt(seconds);
+        timerText.text = minutes + " : " + roundSeconds;
     }
 }
+
